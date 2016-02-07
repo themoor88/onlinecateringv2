2abc80eb9b180bff5ea4b71609a059bfe1a4cbb5
WajidKagzi <wjkagzi@gmail.com>
2016-02-07 11:48:52 -0500

    Adding more images and modifying templates.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.precompile += %w( example-1.jpg example-2 example-3.jpg example-4.jpg example-5.jpg example-6.jpg )
+Rails.application.config.assets.precompile += %w( example-1.jpg example-2 example-3.jpg example-4.jpg example-5.jpg example-6.jpg howitworks.png map.jpg avatar_01.jpg)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
