b19a4f1d65b20419e4e76409534a55b79e0e38e8
WajidKagzi <wjkagzi@gmail.com>
2016-02-13 23:09:04 -0500

    Adding extra size for paperclip.

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -30,7 +30,8 @@ class FoodItem < ActiveRecord::Base
   has_attached_file :photo, styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x300>'
+    medium: '300x300>',
+    large: '500x500>'
   }
 
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
